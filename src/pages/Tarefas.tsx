7af0c9408891131a76db9c9a8503284da0a381a6
brunohro <brunoh.rodrigues27@gmail.com>
2025-05-04 17:54:15 -0300

    Add animation to history and task pages

--- src/pages/Tarefas.tsx
+++ src/pages/Tarefas.tsx
@@ -1,24 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import { motion } from "framer-motion";
 
 function Tarefas() {
   const navigate = useNavigate();
   return (
     <div className="bg-zinc-800 min-h-screen text-white px-6 pt-4">
       <Header />
-      <div className="text-center max-w-md mx-auto rounded-lg py-10">
-        <h2>Essa é a página de tarefas</h2>
-        <p>Ainda está sendo produzida!</p>
-        <p>...</p>
-        <div>
-          <button
-            onClick={() => navigate("/")}
-            className="py-4 text-left text-red-400 hover:text-red-600 transition"
-          >
-            Voltar para tela inicial
-          </button>
+      <motion.div
+        className="min-h-screen text-white p-6"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.0 }}
+      >
+        <div className="text-center max-w-md mx-auto rounded-lg py-10">
+          <h2>Essa é a página de tarefas</h2>
+          <p>Ainda está sendo produzida!</p>
+          <p>...</p>
+          <div>
+            <button
+              onClick={() => navigate("/")}
+              className="py-4 text-left text-red-400 hover:text-red-600 transition"
+            >
+              Voltar para tela inicial
+            </button>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
